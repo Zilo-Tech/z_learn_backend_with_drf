30377e1933807f8ab4831d07ce1a7e8508651658
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-10-31 11:12:46 +0100

    changing from modelviewsets to viewset. Now post request of post not working

--- z_learn/chat_section/api/views.py
+++ z_learn/chat_section/api/views.py
@@ -11,22 +11,38 @@ from drf_yasg.utils import swagger_auto_schema
 from .serializers import PostSerializer, CommentSerializer
 from chat_section.models import Post, Comment
 from .permissions import PostUserOrNot
+# from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class PostViewSet(viewsets.ViewSet):
+    # queryset = Post.objects.all()
+    # serializer_class = PostSerializer
     permission_classes = [PostUserOrNot]
     
     
+    # @extends_schema(
+    #     description="Create a new post request",
+    #     request = PostSerializer,
+    #     response=PostSerializer 
+    # )
+    def create(self, request, *args, **kwargs):
+        """ Handle POST requests to create a Post for a user """
+        user = request.user 
+        post_question = Post.objects.create(post_user = user)
+        print(f"The post questio is {post_question}")
+        serializer = PostSerializer(post_question)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        
+        
     # def perform_create(self, serializer):
     #     login_user = self.request.user
     #     print(login_user)
     #     return serializer.save(post_user=login_user) 
     
-    def perform_create(self, serializer):
-        return serializer.save(post_user=self.request.user)
+    # def perform_create(self, serializer):
+    #     return serializer.save(post_user=self.request.user)
     
     
-    def perform_update(self, serializer):
-        return serializer.save(post_user=self.request.user)
+    # def perform_update(self, serializer):
+    #     return serializer.save(post_user=self.request.user)
