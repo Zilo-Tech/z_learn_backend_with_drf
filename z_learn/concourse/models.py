719ffdef3cc1e88d842f171c899bfdf50b4bbb99
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-11-05 18:01:03 +0100

    starting working on Concourse app

--- z_learn/concourse/models.py
+++ z_learn/concourse/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Concourse(models.Model):
+    concourseName = models.CharField(max_length=100, blank=False, null=False)
+    concourseSubName = models.CharField(max_length=100, blank=True, null=True)
+    activeUsers = models.IntegerField(default=0)
+    price = models.IntegerField()
+    description = models.TextField()
+    
+
+
+class ConcoursesDepartment(models.Model):
+    departmentName = models.CharField(max_length=100, blank=False, null=False)
+    departmentConcourse = models.ForeignKey(Concourse, on_delete=models.CASCADE, related_name="departments")
+    
+    
+
+class LatestNews(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    
+    
+    
