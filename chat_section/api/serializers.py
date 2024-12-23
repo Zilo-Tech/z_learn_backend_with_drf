7bf25aa97e5663785a5348bcb976ddfb1f246503
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-12-23 16:23:45 +0100

    returns string representation of category in post

--- chat_section/api/serializers.py
+++ chat_section/api/serializers.py
@@ -12,10 +12,11 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     post_user = serializers.StringRelatedField(source='post_user.username', read_only=True)
-
+    category = serializers.StringRelatedField(source='category.name', read_only=True)
     class Meta:
         model = Post
-        fields = "__all__"
+        # fields = "__all__"
+        fields = ["id", "title", "content", "image_post", "upvotes", "downvotes", "views", "date_created", "post_user", "comments", "category"]
       
 
 class CategorySerializer(serializers.ModelSerializer):
