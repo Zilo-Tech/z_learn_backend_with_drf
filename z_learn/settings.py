ca0352023c4ca7fc558c3c5e61ec449b03c5fd1e
NyuydineBill <billleynyuy@gmail.com>
2025-01-03 23:29:48 +0100

    Added S3 settings

--- z_learn/settings.py
+++ z_learn/settings.py
@@ -57,7 +57,9 @@ INSTALLED_APPS = [
     'authentication',
     'annoucement_news',
     'chat_section',
-    'concourse'
+    'concourse',
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -145,7 +147,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -178,5 +179,44 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PagePagination',
 #     'PAGE_SIZE': 2
 # }
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AWS S3 settings
+from decouple import config
+
+# AWS S3 settings from .env
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+
+# Other optional settings
+AWS_S3_FILE_OVERWRITE = False  # Prevent overwriting files with the same name
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+
+# Use the S3Boto3Storage backend for media files
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "access_key": AWS_ACCESS_KEY_ID,
+            "secret_key": AWS_SECRET_ACCESS_KEY,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "access_key": AWS_ACCESS_KEY_ID,
+            "secret_key": AWS_SECRET_ACCESS_KEY,
+        },
+    },
+}
+AWS_FOLDER = 'ZLearn'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_FOLDER}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files (uploaded files)
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_FOLDER}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
