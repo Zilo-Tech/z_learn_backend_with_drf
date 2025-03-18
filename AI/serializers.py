4137b83b6f9ec7a685a2b55368fb253d25a6aa55
NyuydineBill <billleynyuy@gmail.com>
2025-03-18 02:10:23 +0100

    Implemented AI questions chatbot using gemini

--- AI/serializers.py
+++ AI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Chat
+from .models import Chat, ChatGPTInteraction
 
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +12,9 @@ class ChatSerializer(serializers.ModelSerializer):
         query = validated_data.get('query')
         response = validated_data.get('response')
         return Chat.objects.create(user=user, query=query, response=response)
+
+class ChatGPTInteractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatGPTInteraction
+        fields = ['id', 'user', 'query', 'response', 'timestamp']
+        read_only_fields = ['user', 'response']
